60c7b227a2ca0815c861ca9ed42b6a9244097312
Matheus Diôgo Alves da Silva <matheusdiogoeng@gmail.com>
2023-07-10 17:39:17 -0300

    Adicionando download do banco via google sheets

--- millennium of macroeconomic.py
+++ millennium of macroeconomic.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Recebendo dados do google sheets
+URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQSv10S9cssxOoBF2QGYNukBK-vCgB8fVkhqwtj5n0ASD0OLVBpF15lKakXNUia-90CXokHd47r572c/pub?output=xlsx'
+
+df = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A2. Pop of Eng & GB 1086-1870', skiprows = range(0, 7)))
 
