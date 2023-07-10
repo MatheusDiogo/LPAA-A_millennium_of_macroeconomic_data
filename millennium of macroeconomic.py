8874d404603de120fa1386f4e32c63bebb8ab6ba
Matheus Diôgo Alves da Silva <matheusdiogoeng@gmail.com>
2023-07-10 17:42:54 -0300

    Aplicando tratamento e filtro de dados
    Recebe apenas dados a partir da linha 7 pois acima disso tem texto de introdução que não será utilizado nesse projeto

--- millennium of macroeconomic.py
+++ millennium of macroeconomic.py
@@ -6,3 +6,11 @@ URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQSv10S9cssxOoBF2QGYNukBK
 
 df = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A2. Pop of Eng & GB 1086-1870', skiprows = range(0, 7)))
 
+#Removendo dados Nan do banco de dados
+df['Year'].dropna(inplace = True)
+df['Population of England, millions'].fillna('0', inplace = True)
+df['Population of Great Britain, millions'].fillna('0', inplace = True)
+df['Year'] = df['Year'].astype(float)
+df['Population of England, millions'] = df['Population of England, millions'].astype(float)
+df['Population of Great Britain, millions'] = df['Population of Great Britain, millions'].astype(float)
+df.reset_index(drop = True, inplace= True)
