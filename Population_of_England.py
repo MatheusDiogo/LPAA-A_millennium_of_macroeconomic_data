e06876234c60dde829589ef523dab3db2602482b
Matheus Diôgo Alves da Silva <matheusdiogoeng@gmail.com>
2023-07-11 15:41:03 -0300

    Update Population_of_England.py
    Adicionando marcador no gráfico representando guerra dos cem anos

--- Population_of_England.py
+++ Population_of_England.py
@@ -21,6 +21,7 @@ cols = cols.map(lambda x: x.replace(',', ' in') if isinstance(x, (str)) else x)
 cols = cols.map(lambda x: x.replace(' ', '_') if isinstance(x, (str)) else x)
 df.columns = cols
 
+#Plotando gráfico de população por ano
 filtroBritain = pd.DataFrame(df.drop(df.query('Population_of_Great_Britain_in_millions == 0').index))
 filtroBritain.reset_index(drop = True, inplace = True)
 plt.plot(filtroBritain['Year'], filtroBritain['Population_of_Great_Britain_in_millions'], label='População da Grã-Bretanha')
@@ -30,3 +31,13 @@ plt.ylabel('População (milhões)')
 plt.title('População ao Longo do Tempo')
 plt.legend()
 
+# Adicionando Marcadores da Guerra dos cem anos
+year_1337_index = df[df['Year'] == 1337].index[0]
+year_1453_index = df[df['Year'] == 1453].index[0]
+plt.scatter(df['Year'][year_1337_index], df['Population_of_England_in_millions'][year_1337_index], color='k', marker='o', facecolors='none', edgecolors='k', linewidths=2)
+plt.scatter(df['Year'][year_1453_index], df['Population_of_England_in_millions'][year_1453_index], color='k', marker='o', facecolors='none', edgecolors='k', linewidths=2)
+plt.annotate('Guerra dos cem anos', xy=(df['Year'][year_1337_index], df['Population_of_England_in_millions'][year_1337_index]), xytext=(df['Year'][year_1337_index]-55, df['Population_of_England_in_millions'][year_1337_index]+3), color='k')
+plt.annotate('(1337)', xy=(df['Year'][year_1337_index], df['Population_of_England_in_millions'][year_1337_index]), xytext=(df['Year'][year_1337_index]-37, df['Population_of_England_in_millions'][year_1337_index]+1), color='k')
+plt.annotate('(1453)', xy=(df['Year'][year_1453_index], df['Population_of_England_in_millions'][year_1453_index]), xytext=(df['Year'][year_1453_index]-37, df['Population_of_England_in_millions'][year_1453_index]+1), color='k')
+
+plt.show()
