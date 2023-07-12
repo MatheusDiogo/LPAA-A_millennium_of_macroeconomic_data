8d6d0d374aea94d76dfdde3fbaea49b28b1db1bc
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-12 11:56:39 -0300

    Adicionando gráfico da produção agrícola por ano

--- Agriculture_England.py
+++ Agriculture_England.py
@@ -9,9 +9,22 @@ agri_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 12
 rebanhos_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, Y:AA'))
 pecuaria_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, AC:AJ'))
 
+#Removendo linhas contendo unidades
 agri_eng.drop(index = [0], inplace = True)
 agri_eng.reset_index(drop = True, inplace = True)
 rebanhos_eng.drop(index = [0], inplace = True)
 rebanhos_eng.reset_index(drop = True, inplace = True)
 pecuaria_eng.drop(index = [0], inplace = True)
 pecuaria_eng.reset_index(drop = True, inplace = True)
+
+#Plotando Gráfico de Série das produções agriculas
+plt.plot(agri_eng['Year'], agri_eng['Wheat.1'], label='Trigo')
+plt.plot(agri_eng['Year'], agri_eng['Rye.1'], color='r', label='Centeio')
+plt.plot(agri_eng['Year'], agri_eng['Barley.1'], color='k', label='Cevada')
+plt.plot(agri_eng['Year'], agri_eng['Oats.1'], color='b', label='Aveia')
+plt.plot(agri_eng['Year'], agri_eng['Pulses.1'], color='y', label='Pulses')
+plt.plot(agri_eng['Year'], agri_eng['Potatoes.1'], color='g', label='Tomate')
+plt.xlabel('Year')
+plt.ylabel('Production in million bushels)')
+plt.title('Produção Agricula Na Inglaterra')
+plt.legend()
