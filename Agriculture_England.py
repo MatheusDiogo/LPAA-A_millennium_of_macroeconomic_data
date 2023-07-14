5dd4f2cd5d4e4047d5f6fb452c8ca5b3c7251c67
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-14 10:12:24 -0300

    Removendo dados de rebanho e pecuária para análise independente

--- Agriculture_England.py
+++ Agriculture_England.py
@@ -6,16 +6,10 @@ URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQSv10S9cssxOoBF2QGYNukBK
 
 #Recebendo dados e criando DataFrames
 agri_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, Q:V'))
-rebanhos_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, Y:AA'))
-pecuaria_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, AC:AJ'))
 
 #Removendo linhas contendo unidades
 agri_eng.drop(index = [0], inplace = True)
 agri_eng.reset_index(drop = True, inplace = True)
-rebanhos_eng.drop(index = [0], inplace = True)
-rebanhos_eng.reset_index(drop = True, inplace = True)
-pecuaria_eng.drop(index = [0], inplace = True)
-pecuaria_eng.reset_index(drop = True, inplace = True)
 
 #Plotando Gráfico de Série das produções agriculas
 plt.figure(figsize = ((19.5, 12))))
