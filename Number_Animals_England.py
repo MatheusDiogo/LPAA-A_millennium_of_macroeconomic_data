ec659c878b62e4f1bb413b4786f31f2d4b98983a
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-14 12:40:37 -0300

    Apresentando gráfico de comparação
    Apresentando gráfico de comparação junto aos outros 3 gráficos

--- Number_Animals_England.py
+++ Number_Animals_England.py
@@ -12,29 +12,29 @@ rebanhos_eng.drop(index = [0], inplace = True)
 rebanhos_eng.reset_index(drop = True, inplace = True)
 
 #Plotando Gráfico de Série das produções agriculas
-plt.figure(figsize = ((19.5, 4)))
-plt.subplot(1, 3, 1)
+plt.figure(figsize = ((12, 6)))
+plt.subplot(2, 2, 1)
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Cattle'], label='Gado')
 plt.title('Gado')
 plt.xlabel('Year')
 
-plt.subplot(1, 3, 2)
+plt.subplot(2, 2, 2)
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Sheep'], color='r', label='Ovelha')
 plt.title('Ovelha')
 plt.xlabel('Year')
 
-plt.subplot(1, 3, 3)
+plt.subplot(2, 2, 3)
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Pigs'], color='k', label='Porco')
 plt.title('Porco')
 plt.xlabel('Year')
-
-plt.suptitle("Number of animals in millions")
-plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.8, wspace=0.2, hspace=0.7)
 
 #Gráfico único
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Cattle'], label='Gado')
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Sheep'], color='r', label='Ovelha')
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Pigs'], color='k', label='Porco')
+plt.title('Comparação')
 plt.xlabel('Year')
 plt.legend()
+
 plt.suptitle("Number of animals in millions")
+plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.88, wspace=0.15, hspace=0.35)
