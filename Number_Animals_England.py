9b0c52f7819b344828d33a5b48ec8d3c7342bb0c
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-14 10:11:33 -0300

    Create Number_Animals_England.py
    Criando arquivo para gráfico dos rebanhos da Inglaterra

---
+++ Number_Animals_England.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Recebendo dados do google sheets
+URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQSv10S9cssxOoBF2QGYNukBK-vCgB8fVkhqwtj5n0ASD0OLVBpF15lKakXNUia-90CXokHd47r572c/pub?output=xlsx'
+
+rebanhos_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, Y:AA'))
+
+rebanhos_eng.drop(index = [0], inplace = True)
+rebanhos_eng.reset_index(drop = True, inplace = True)
+
+plt.figure(figsize = ((19.5, 4)))
+plt.subplot(1, 3, 1)
+plt.plot(rebanhos_eng['Year'], rebanhos_eng['Cattle'], label='Gado')
+plt.title('Gado')
+plt.xlabel('Year')
+
+plt.subplot(1, 3, 2)
+plt.plot(rebanhos_eng['Year'], rebanhos_eng['Sheep'], color='r', label='Ovelha')
+plt.title('Ovelha')
+plt.xlabel('Year')
+
+plt.subplot(1, 3, 3)
+plt.plot(rebanhos_eng['Year'], rebanhos_eng['Pigs'], color='k', label='Porco')
+plt.title('Porco')
+plt.xlabel('Year')
+
+plt.suptitle("Number of animals in millions")
+plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.8, wspace=0.2, hspace=0.7)
