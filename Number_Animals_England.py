7b648eef8285eec91705a043de918be3cd2c5ff1
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-14 10:13:41 -0300

    Adicionando comentários

--- Number_Animals_England.py
+++ Number_Animals_England.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 #Recebendo dados do google sheets
 URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQSv10S9cssxOoBF2QGYNukBK-vCgB8fVkhqwtj5n0ASD0OLVBpF15lKakXNUia-90CXokHd47r572c/pub?output=xlsx'
 
+#Recebendo dados e criando DataFrames
 rebanhos_eng = pd.DataFrame(pd.read_excel(URL, sheet_name = 'A3. Eng. Agriculture 1270-1870', skiprows = range(0, 10), usecols = 'A, Y:AA'))
 
+#Removendo linhas contendo unidades
 rebanhos_eng.drop(index = [0], inplace = True)
 rebanhos_eng.reset_index(drop = True, inplace = True)
 
+#Plotando Gráfico de Série das produções agriculas
 plt.figure(figsize = ((19.5, 4)))
 plt.subplot(1, 3, 1)
 plt.plot(rebanhos_eng['Year'], rebanhos_eng['Cattle'], label='Gado')
